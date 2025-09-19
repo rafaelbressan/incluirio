899d855a7a6677522ad28f878b79f9449925f14d
Rafael Bressan <bressan.rafa@gmail.com>
2025-09-19 20:02:57 -0300

    Refactor code structure for improved readability and maintainability

---
+++ src/_data/sponsors.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    file: "rotary-doisirmaos.png",
+    name: "Rotary Dois Irmãos",
+    type: "rotary",
+    width: 300
+  },
+  {
+    file: "rotary-fundacaosocios.png",
+    name: "Rotary Fundação Sócios",
+    type: "rotary",
+    width: 300
+  },
+  {
+    file: "rotary-jacarepagua.png",
+    name: "Rotary Jacarepaguá",
+    type: "rotary",
+    width: 300
+  },
+  {
+    file: "rotary-leblongavea.png",
+    name: "Rotary Leblon Gávea",
+    type: "rotary",
+    width: 300
+  },
+  {
+    file: "rotary-riodejaneiro.png",
+    name: "Rotary Rio de Janeiro",
+    type: "rotary",
+    width: 300
+  },
+  {
+    file: "rotary-sernambetiba.png",
+    name: "Rotary Sernambetiba",
+    type: "rotary",
+    width: 300
+  }
+];
