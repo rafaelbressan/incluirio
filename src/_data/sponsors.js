af2194cca6805539e8f46c1676de27acf76a41f3
Rafael Bressan <bressan.rafa@gmail.com>
2025-09-22 16:47:17 -0300

    Refactor styles: Consolidate CSS files and update Tailwind configuration for improved utility management and responsiveness.

--- src/_data/sponsors.js
+++ src/_data/sponsors.js
@@ -3,36 +3,42 @@ export default [
     file: "rotary-doisirmaos.png",
     name: "Rotary Dois Irmãos",
     type: "rotary",
-    width: 300
+    height: 300,
+    gridFr: 1
   },
   {
     file: "rotary-fundacaosocios.png",
     name: "Rotary Fundação Sócios",
     type: "rotary",
-    width: 300
+    height: 300,
+    gridFr: 2
   },
   {
     file: "rotary-jacarepagua.png",
     name: "Rotary Jacarepaguá",
     type: "rotary",
-    width: 300
+    height: 300,
+    gridFr: 1
   },
   {
     file: "rotary-leblongavea.png",
     name: "Rotary Leblon Gávea",
     type: "rotary",
-    width: 300
+    height: 300,
+    gridFr: 1
   },
   {
     file: "rotary-riodejaneiro.png",
     name: "Rotary Rio de Janeiro",
     type: "rotary",
-    width: 300
+    height: 300,
+    gridFr: 1
   },
   {
     file: "rotary-sernambetiba.png",
     name: "Rotary Sernambetiba",
     type: "rotary",
-    width: 300
+    height: 300,
+    gridFr: 1
   }
 ];
