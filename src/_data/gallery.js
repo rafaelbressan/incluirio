134d656402aed224982b44331f8899380f2b6a63
Rafael Bressan <bressan.rafa@gmail.com>
2025-09-29 23:56:22 -0300

    Refactor code structure for improved readability and maintainability, added carousel and about area

---
+++ src/_data/gallery.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    file: "rotary-incluirio-gallery-01.jpg",
+    alt: "Inclui Rio - Participantes da corrida 2023"
+  },
+  {
+    file: "rotary-incluirio-gallery-02.jpg",
+    alt: "Inclui Rio - Momento de superação na corrida"
+  },
+  {
+    file: "rotary-incluirio-gallery-03.jpg",
+    alt: "Inclui Rio - Comunidade unida pela inclusão"
+  },
+  {
+    file: "rotary-incluirio-gallery-04.jpg",
+    alt: "Inclui Rio - Celebração da diversidade"
+  },
+  {
+    file: "rotary-incluirio-gallery-05.jpg",
+    alt: "Inclui Rio - Corrida pela causa inclusiva"
+  },
+  {
+    file: "rotary-incluirio-gallery-06.jpg",
+    alt: "Inclui Rio - Unidos por uma cidade acessível"
+  },
+  {
+    file: "rotary-incluirio-gallery-07.jpg",
+    alt: "Inclui Rio - Transformando vidas através do esporte"
+  }
+];
